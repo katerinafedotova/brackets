a36920a2ff27c39cb5aeb4721c556320be1614c5
Ekaterina <fedotova490@gmail.com>
2020-10-17 17:53:58 +0300

    feat: add solution to brackets-task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length === 0) return true
+  if (str.length % 2 !==0) return false
+
+  let config = bracketsConfig.toString().split(',')
+
+  const isValid = (a,b) => {
+      for (let i = 0; i < config.length; i += 2){
+         if (a === config[i] && b === config[i+1]) return true
+      }
+      return false 
+  }
+    
+  let array = str.split('')
+  let n = array.length
+  for (let i = 0; i < n-1; i++){
+    if (isValid(array[i], array[i+1])) {
+      array.splice(i, 2) 
+      return check(array.join(''), bracketsConfig)
+    }
+  }
+  return false
 }
